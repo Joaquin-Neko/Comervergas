3938c0b341b1441aa14b815c3ca04d30da71ef83
sebastian120718 <jsgarciag2@academia.usbbog.edu.co>
2026-05-03 03:48:23 +0000

    final

--- macota.java
+++ macota.java
@@ -4,7 +4,38 @@ public class macota{
     private String nombre;
 }
 
-public macota{int edad, int estatura, String nombre};
-this.edad=edad;
-this.estatura=estatura;
-this.nombre=nombre
+public macota(int edad, int estatura, String nombre){
+    this.edad=edad;
+    this.estatura=estatura;
+    this.nombre=nombre;
+}
+
+public int getEdad(){
+    return edad;
+}
+
+public void setEdad (int edad){
+    this.edad=edad;
+}
+
+public int getEstatura(){
+    return estatura;
+}
+public void setEstatura(int estatura){
+    this.estatura=estatura;
+}
+public String getNombre(){
+    return nombre;
+}
+public void setNombre(String nombre){
+    this.nombre=nombre;
+
+}
+
+public void mostrarInfo(){
+        System.out.println("Informacionde de una mascota");
+        System.out.println("edad: " + getEdad());
+        System.out.println("estatura: " + getEstatura());
+        System.out.println("estatura: " + getNombre());
+}
+
