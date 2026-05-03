eb6478cd987de62e7a029e845443f1a2b491cd81
sebastian120718 <jsgarciag2@academia.usbbog.edu.co>
2026-05-03 03:32:38 +0000

    ejemplo

---
+++ macota.java
@@ -0,0 +1,10 @@
+public class macota{
+    private int edad;
+    private int estatura;
+    private String nombre;
+}
+
+public macota{int edad, int estatura, String nombre};
+this.edad=edad;
+this.estatura=estatura;
+this.nombre=nombre
